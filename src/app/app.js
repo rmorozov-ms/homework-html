21a3d7ea2650dc23936016a47c8a7c5dd60d3ced
rmorozov <rmorozov@moysklad.ru>
2025-04-28 10:06:29 +0300

    homework_001

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,9 @@
 import html from "./app.html";
 import './app.css'
+import { fillTable } from './tableProcessor.js'
 
 
 const rootElement = document.getElementById('root');
 rootElement.innerHTML = html;
+
+fillTable();
